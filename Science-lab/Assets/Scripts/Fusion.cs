bb4a6c0bb163d9499ddd6d47966d1e1fca1aa882
JasperDre <j.drescher@cortexarts.com>
2016-10-09 16:17:19 +0200

    Added fusion and changed light

---
+++ Science-lab/Assets/Scripts/Fusion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Fusion : MonoBehaviour
+{
+    public GameObject Gunpowder;
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+            if (col.gameObject.name == "Fire" && gameObject.name == "Potassiumnitrate")
+            {
+                Gunpowder.transform.position = gameObject.transform.position;
+                Instantiate(Gunpowder, new Vector2(Gunpowder.transform.position.x, Gunpowder.transform.position.y), Quaternion.identity);
+                Destroy(col.gameObject);
+                Destroy(gameObject);
+        }
+     }
+}
