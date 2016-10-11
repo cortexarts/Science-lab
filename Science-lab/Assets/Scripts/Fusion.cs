9aabe0eaed18c6fd5205c34dad111c7f53d11d54
JasperDre <j.drescher@cortexarts.com>
2016-10-11 14:56:20 +0200

    Added hydrogen peroxide fusion

--- Science-lab/Assets/Scripts/Fusion.cs
+++ Science-lab/Assets/Scripts/Fusion.cs
@@ -2,16 +2,31 @@
 
 public class Fusion : MonoBehaviour
 {
+    public GameObject PopUp;
     public GameObject Gunpowder;
+    public GameObject Hydrogenperoxide;
+    public GameObject Energy;
 
     void OnTriggerEnter2D(Collider2D col)
     {
-            if (col.gameObject.name == "Fire" && gameObject.name == "Potassiumnitrate")
-            {
+        if (col.gameObject.name == "Fire" && gameObject.name == "Potassiumnitrate") //Create Charcoal
+        {
                 Gunpowder.transform.position = gameObject.transform.position;
                 Instantiate(Gunpowder, new Vector2(Gunpowder.transform.position.x, Gunpowder.transform.position.y), Quaternion.identity);
                 Destroy(col.gameObject);
                 Destroy(gameObject);
         }
-     }
+        else if (col.gameObject.name == "Hydrogen" && gameObject.name == "Oxygen") //Create Hydrogen peroxide and energy
+        {
+            Hydrogenperoxide.transform.position = gameObject.transform.position;
+            Vector3 Energyposition = transform.position;
+            Energyposition.x += 90;
+            Energy.transform.position = Energyposition;
+            Instantiate(Hydrogenperoxide, new Vector2(Hydrogenperoxide.transform.position.x, Hydrogenperoxide.transform.position.y), Quaternion.identity);
+            Instantiate(Energy, new Vector2(Energy.transform.position.x, Energy.transform.position.y), Quaternion.identity);
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+            PopUp.SetActive(true);
+        }
+    }
 }
